0af9bf8f079f9643b7cf291b01929d152dcaa267
M-Ainurridho <m.ainurridho11@gmail.com>
2024-12-19 16:54:49 +0700

    layouting

---
+++ src/layout/index.jsx
@@ -0,0 +1,14 @@
+import Modal from "../components/modals";
+import Navbar from "../components/navbar";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Modal />
+    </>
+  );
+};
+
+export default Layout;
